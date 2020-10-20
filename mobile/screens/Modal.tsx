6196d01260a41c54151b1527677c83144ac15513
TomasPabloFerreira <tomasferreira373@gmail.com>
2020-10-20 02:59:21 -0300

    feat(mobile): fetch and show data in modal screen

--- mobile/screens/Modal.tsx
+++ mobile/screens/Modal.tsx
@@ -1,10 +1,37 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
+import useFetch from '../hooks/useFetch'
+import Meal from '../types/Meal'
 
-export default () => {
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
+})
+
+type ModalProps = { route: any, navigation: any }
+
+const Modal = ({ route, navigation } : ModalProps) => {
+
+	const id = route.params._id
+	
+	const { loading, data: meal }: { loading: boolean, data: Meal } =
+		useFetch(`https://lunchly-api.tomasferreira.vercel.app/api/meals/${id}`)
+	
 	return (
-		<View>
-			<Text>MODAL</Text>
+		<View style={styles.container}>
+			{ loading
+				? <Text>loading...</Text>
+				:
+				<>
+					<Text>{meal.name}</Text>
+					<Text>{meal.description}</Text>
+				</>
+			}
 		</View>
 	)
 }
+
+export default Modal
